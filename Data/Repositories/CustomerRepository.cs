a7229567dffac19c8722c3b045a7736b00b246ba
Alican Mavi <mavia240@gmail.com>
2025-02-17 16:28:54 +0100

    Add project files.

---
+++ Data/Repositories/CustomerRepository.cs
@@ -0,0 +1,53 @@
+﻿using Data.Contexts;
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repositories;
+
+public class CustomerRepository(DataContext context)
+{
+    private readonly DataContext _context = context;
+
+    //Create
+    public async Task<CustomerEntity> Createasync(CustomerEntity customer)
+    {
+        _context.Customers.Add(customer);
+        await _context.SaveChangesAsync();
+        return customer;
+    }
+    //Read
+    public async Task<IEnumerable<CustomerEntity>> GetAllAsync()
+    {
+        return await _context.Customers.ToListAsync();
+    }
+    //Update
+    public async Task<CustomerEntity?> UpdateAsync(CustomerEntity updatedcustomer)
+    {
+        var existingCustomer = await _context.Customers.FirstOrDefaultAsync(c => c.Id  == updatedcustomer.Id);
+        if (existingCustomer != null)
+        {
+            existingCustomer.CustomerName = updatedcustomer.CustomerName;
+            existingCustomer.CustomerEmail = updatedcustomer.CustomerEmail;
+            existingCustomer.CustomerPhoneNumber = updatedcustomer.CustomerPhoneNumber;
+
+            await _context.SaveChangesAsync();
+            return existingCustomer;
+        }
+        return null;
+    }
+    //Delete klar
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var entity = await _context.Customers.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (entity != null)
+        {
+            _context.Customers.Remove(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        return false;
+    }
+}
+
+
